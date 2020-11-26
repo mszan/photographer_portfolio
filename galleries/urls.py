ef4fa39c97a4007ff789682c7b598d0c75fd0559
Dawid Mszanowski <dmszanowski@icloud.com>
2020-11-26 01:40:09 +0100

    Update landing page and bio

--- galleries/urls.py
+++ galleries/urls.py
@@ -1,14 +1,17 @@
 from django.shortcuts import redirect
 from django.urls import path
-from galleries.views import GallerySmallView, GalleryBigView
+from galleries.views import GallerySmallView, GalleryBigView, GalleryLandingView
 from .models import Gallery
 
 
 urlpatterns = [
     # Landing url that redirect to first found gallery with index 0.
-    path('',
-         lambda request: redirect(f'galleries/{Gallery.objects.filter(main=True).first().id}/big/', permanent=False),
-         name='galleries-landing'),
+    # path('',
+    #      lambda request: redirect(f'galleries/{Gallery.objects.filter(main=True).first().id}/big/', permanent=False),
+    #      name='galleries-landing'),
+
+    # Landing url.
+    path('', GalleryLandingView.as_view(), name='landing'),
 
     # Big gallery url.
     path('galleries/<int:pk>/big/', GalleryBigView.as_view(), name='galleries-big'),
